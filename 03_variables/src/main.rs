de97a15a83f20e2080aef5b1e943e371acfe5515
Naginipython <naginipython332@gmail.com>
2023-07-03 20:56:47 -0400

    messed with variables and functions

--- 03_variables/src/main.rs
+++ 03_variables/src/main.rs
@@ -15,5 +15,25 @@ fn main() {
     println!("Amount of seconds in 3 hours constant: {AMOUNT_OF_SECONDS_IN_THREE_HOURS}");
     println!("How long is the spaces variable: {spaces}");
 
-    let tup: (String, i32) = ("Test", 1234);
+    let tup: (&str, i32) = ("Test", 1234);
+
+    let float: f64 = 6.24;
+
+    println!("Tuple in action: {}, {}. Float in action: {float}", tup.0, tup.1);
+
+    let _arr = [3; 5]; //array of five 3's
+    let _barr: [i32; 3] = [1, 2, 3];
+    
+    let _y = {
+        let x = 3;
+        x + 1 //NO SEMICOLON: makes it a statement, and is not returned
+    };
+
+    let x = plus_one(5);
+    println!("The inline function number: {x}");
+}
+
+//Rust prefers snake case, aka lower case with indents
+fn plus_one(x: i32) -> i32 {
+    x + 1 //I CAN return, but if I don't use a semicolon, its not necessary
 }
--- 03_variables/target/debug/variables.d
+++ 03_variables/target/debug/variables.d
@@ -1,1 +1,1 @@
-/home/ben/Code/Rust/03_variables/target/debug/variables: /home/ben/Code/Rust/03_variables/src/main.rs
+/home/ben/Code/Rust/BeginnerRustProjects/03_variables/target/debug/variables: /home/ben/Code/Rust/BeginnerRustProjects/03_variables/src/main.rs
