e17614b2297ef712e8591ec2598173eb42bd8374
Naginipython <naginipython332@gmail.com>
2023-08-09 20:53:55 -0400

    Slightly changed 08.2 text

--- 08.2_collections_practice/src/company_list.rs
+++ 08.2_collections_practice/src/company_list.rs
@@ -12,14 +12,14 @@ pub fn company_list() {
     //3. Using a hash map and vectors, create a text interface to allow a user to add employee names to a department in a company. For example, “Add Sally to 
     //  Engineering” or “Add Amir to Sales.” Then let the user retrieve a list of all people in a department or all people in the company by department, sorted 
     //  alphabetically.
-    println!("Welcome to your employee organizer!
-To use this application, please type in one of the following commands:
-    view (commands/employees/departments/[deparment])
-    add [name] to [department]
-    add department [department name]
-    remove [name] from [department]
-    remove department [department]
-    exit app");
+    println!("Welcome to your employee organizer!\n\
+        To use this application, please type in one of the following commands:\n\
+        \tview (commands/employees/departments/[deparment])\n\
+        \tadd [name] to [department]\n\
+        \tadd department [department name]\n\
+        \tremove [name] from [department]\n\
+        \tremove department [department]\n\
+        \texit app");
 
     let mut departments: Vec<String> = Vec::new();
     let mut employees: HashMap<String, String> = HashMap::new();
@@ -181,12 +181,12 @@ To use this application, please type in one of the following commands:
                                 println!("Employees: {:?}", output);
                             },
                             "commands" => {
-                                println!("    view (commands/employees/[deparment])
-    add [name] to [department]
-    add department [department name]
-    remove [name] from [department]
-    remove department [department]
-    exit app");
+                                println!("\tview (commands/employees/[deparment])\n\
+                                    \tadd [name] to [department]\n\
+                                    \tadd department [department name]\n\
+                                    \tremove [name] from [department]\n\
+                                    \tremove department [department]\n\
+                                    \texit app");
                             },
                             "departments" => {
                                 departments.sort();
